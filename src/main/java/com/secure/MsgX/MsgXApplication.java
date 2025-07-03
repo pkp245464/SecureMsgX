80d3e85ba333b7b87c14ad3c766bc873ae9ca04e
Pankaj Kumar <pankajkumar245464@gmail.com>
2025-07-03 16:16:44 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/secure/MsgX/MsgXApplication.java
@@ -0,0 +1,13 @@
+package com.secure.MsgX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure/MsgX/MsgXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.MsgX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
